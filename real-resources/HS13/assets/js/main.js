ffc2ddf4b6de814da75d56c75d1092e94c14155f
Apladdict <izip14@gmail.com>
2020-03-20 11:38:51 -0500

    added option to import settings

--- real-resources/HS13/assets/js/main.js
+++ real-resources/HS13/assets/js/main.js
@@ -1,3 +1,10 @@
+if (importSettings) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = importSettings;
+    script.type = 'text/javascript';
+    head.append(script);
+}
 if (fontURL) {
     var fontArray = fontURL.split(".");
     var newStyle = document.createElement('style');
