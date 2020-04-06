70f33fdb391bab7ed66840d21576852d42f95df1
Jamie Berghmans <jamie.berghmans@student.kdg.be>
2020-04-06 17:46:49 +0200

    Initial commit

---
+++ package.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+#Checking if there is an argument supplied
+if [[ $# -ne 1 ]]; then
+    echo "Please include a deb"
+    echo "Usage: ./package.sh <path to deb>"
+fi
+
+#Checking if the first argument is a deb
+if [[ ! -f "$1" ]]; then
+    echo "Please include a deb"
+    echo "Usage: ./package.sh <path to deb>"
+fi
+
+#If an earlier extracted package exists, delete the directory and all its files,
+#then create it again and go into it
+if [[ -d "extracted" ]]; then
+    rm -r -f "extracted" > /dev/null
+fi
+
+mkdir "extracted" > /dev/null
+cd "extracted"
+
+#Extract the control and data files
+ar -x "$1" > /dev/null
+tar -xvf "control.tar.gz" > /dev/null
+tar --lzma -xvf "data.tar.lzma" > /dev/null
+
+#Read the control file
+CONTROL=""
+CONTROL_DIRECTORY=""
+if [[ -d "control" ]]; then
+    CONTROL=$(cat "control/control")
+    CONTROL_DIRECTORY="control/control"
+else
+    CONTROL=$(cat "control")
+    CONTROL_DIRECTORY="control"
+fi
+
+#Find the package id in the control file
+PACKAGE_ID=""
+
+while IFS= read -r line; do
+    KEY=${line%%:*}
+    VALUE=${line#*:}
+    if [[ "$KEY" == "Package" ]]; then
+        PACKAGE_ID="$VALUE"
+    fi
+done <<< "$CONTROL"
+
+
+###Find directories to include in delete
+HAS_IWIDGETS=false
+HAS_THEMES=false
+IWIDGETS=""
+THEMES=""
+
+##Check for widgets
+IWIDGETS_DIRECTORY=$(find . -type d | grep -Eo '^.+?\/iWidgets$')
+
+#Find all widget directories
+if [[ -ne "$IWIDGETS_DIRECTORY" ]]; then
+    HAS_IWIDGETS=true
+    IWIDGETS=$(find . -type d | grep -Eo '^.+?\/iWidgets\/[^\/]+?$')
+fi
+
+##Check for themes
+THEMES_DIRECTORY=$(find . -type d | grep -Eo '^.+?\/Themes$')
+
+#Find all widget directories
+if [[ -ne "$THEMES_DIRECTORY" ]]; then
+    HAS_THEMES=true
+    THEMES=$(find . -type d | grep -Eo '^.+?\/Themes\/[^\/]+?$')
+fi
+
+
+#If an earlier extracted DRM version exists, delete the directory and all its files,
+#then create it again and go into it
+cd ..
+
+if [[ -d "troy-drm" ]]; then
+    rm -r -f "troy-drm" > /dev/null
+fi
+
+#Clone template project and enter directory
+git clone --quiet git@github.com:QuixThe2nd/troy-drm.git > /dev/null
+cd troy-drm
+echo "Cloned latest DRM version"
+
+#Remove old resources
+if [[ -d "Resources" ]]; then
+    rm -r -f "Resources" > /dev/null
+fi
+mkdir "Resources"
+
+touch "postinst"
+
+if [[ HAS_IWIDGETS ]]; then
+    echo "mkdir -p /var/mobile/Library/iWidgets" >> "layout/DEBIAN/postinst"
+    echo "cp -r /Library/Application\ Support/Troy/iWidgets.bundle/* ${IWIDGETS_DIRECTORY:1}" >> "layout/DEBIAN/postinst"
+
+    while IFS= read -r line; do
+        VALUE=${line:2}
+        cp -r "../extracted/${line:2}" "Resources/" > /dev/null
+
+        echo "if [[ -d \"${line:1}\" ]]; then" >> "layout/DEBIAN/postinst"
+        echo "chmod -R 775 \"${line:1}\"" >> "layout/DEBIAN/postinst"
+        echo "fi" >> "layout/DEBIAN/postinst"
+
+        echo "if [[ -d \"${line:1}\" ]]; then" >> "layout/DEBIAN/postrm"
+        echo "rm -r \"${line:1}\"" >> "layout/DEBIAN/postrm"
+        echo "fi" >> "layout/DEBIAN/postrm"
+    done <<< "$IWIDGETS"
+fi
+
+if [[ HAS_THEMES ]]; then
+    echo "mkdir -p /Library/Themes" >> "layout/DEBIAN/postinst"
+    echo "cp -r /Library/Application\ Support/Troy/iWidgets.bundle/* ${THEMES_DIRECTORY:1}" >> "layout/DEBIAN/postinst"
+
+    while IFS= read -r line; do
+        VALUE=${line:2}
+        cp -r "../extracted/${line:2}" "Resources/" > /dev/null
+
+        echo "if [[ -d \"${line:1}\" ]]; then" >> "layout/DEBIAN/postinst"
+        echo "chmod -R 775 \"${line:1}\"" >> "layout/DEBIAN/postinst"
+        echo "fi" >> "layout/DEBIAN/postinst"
+
+        echo "if [[ -d \"${line:1}\" ]]; then" >> "layout/DEBIAN/postrm"
+        echo "rm -r \"${line:1}\"" >> "layout/DEBIAN/postrm"
+        echo "fi" >> "layout/DEBIAN/postrm"
+    done <<< "$THEMES"
+fi
+
+echo "rm -r /Library/Application\ Support/Troy" >> "layout/DEBIAN/postinst"
+echo "exit 0" >> "layout/DEBIAN/postinst"
+echo "exit 0" >> "layout/DEBIAN/postrm"
+
+cp "../extracted/$CONTROL_DIRECTORY" "layout/DEBIAN/" > /dev/null
